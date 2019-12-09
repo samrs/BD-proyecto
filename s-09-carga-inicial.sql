57e760f1981bdb29aa40a099a7b1b3251eab922f
alex <alex@alex.thinkpad>
2019-12-09 02:09:59 -0600

    Datos de empleados

--- s-09-carga-inicial.sql
+++ s-09-carga-inicial.sql
@@ -196,3 +196,158 @@ insert into carga
 values
 (seq_aeronave_id.currval, 1, 3.25, 2.45, 15000);
 
+
+
+
+
+
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GODINEZ', 'GONZALEZ', 'ALEJANDRA', 5 , 'GOGA620214VL0', 'GOGA620214MTSDNL11');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'PEREZ', 'OLMOS', 'GONZALO', 3 , 'PEOG6410113N5', 'PEOG641011HYNRLN09');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'RAMOS', 'LOPEZ', 'DAVID', 3 , 'RALD7307254B1', 'RALD730725HHGMPV13');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'LOPEZ', 'JIMENEZ', 'SARA', 4 , 'LOJS680512ZM8', 'LOJS680512MTSPMR12');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'LOPEZ', 'RIOS', 'JAIME', 5 , 'LORJ900914Z2B', 'LORJ900914HTCPSM09');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'PEREZ', 'RIOS', 'LORENA', 2 , 'PERL810921XQM', 'PERL810921MDFRSR06');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'JIMENEZ', 'GONZALEZ', 'DAVID', 1 , 'JIGD861016N58', 'JIGD861016HTLMNV05');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GONZALEZ', 'RIOS', 'CAMILA', 4 , 'GORC650803NW5', 'GORC650803MDGNSM00');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'JIMENEZ', 'RIOS', 'JUAN', 5 , 'JIRJ7607255CY', 'JIRJ760725HNTMSN14');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GONZALEZ', 'JIMENEZ', 'JAIME', 5 , 'GOJJ610312MYL', 'GOJJ610312HNENMM03');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'PEREZ', 'RAMOS', 'ALEJANDRA', 2 , 'PERA790321ELC', 'PERA790321MCCRML15');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'LOPEZ', 'JIMENEZ', 'LORENA', 3 , 'LOJL6508262XJ', 'LOJL650826MQRPMR12');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GONZALEZ', 'OLMOS', 'SARA', 1 , 'GOOS660319JRQ', 'GOOS660319MCHNLR00');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GODINEZ', 'SANCHEZ', 'JORGE', 5 , 'GOSJ690614KQJ', 'GOSJ690614HDGDNR03');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'PEREZ', 'JIMENEZ', 'ALEJANDRA', 2 , 'PEJA9010280ZJ', 'PEJA901028MYNRML03');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'PEREZ', 'PEREZ', 'JORGE', 5 , 'PEPJ6603070HZ', 'PEPJ660307HZSRRR02');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GODINEZ', 'HERNANDEZ', 'CAMILA', 2 , 'GOHC720423AND', 'GOHC720423MASDRM02');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'SANCHEZ', 'GONZALEZ', 'SARA', 2 , 'SAGS650711T2H', 'SAGS650711MSPNNR11');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'RAMOS', 'GODINEZ', 'CAMILA', 3 , 'RAGC7301031R6', 'RAGC730103MHGMDM13');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'OLMOS', 'JIMENEZ', 'CAMILA', 1 , 'OOJC7605247UF', 'OOJC760524MTLLMM13');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'RAMOS', 'GONZALEZ', 'ALEJANDRA', 4 , 'RAGA740827LSO', 'RAGA740827MMSMNL15');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'OLMOS', 'RIOS', 'LORENA', 1 , 'OORL611128DZG', 'OORL611128MGRLSR15');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'RIOS', 'SANCHEZ', 'GONZALO', 2 , 'RISG8012198I0', 'RISG801219HASSNN03');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'OLMOS', 'JIMENEZ', 'GONZALO', 2 , 'OOJG770326R6I', 'OOJG770326HBSLMN09');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GONZALEZ', 'JIMENEZ', 'JORGE', 1 , 'GOJJ8304122OF', 'GOJJ830412HBCNMR07');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'LOPEZ', 'GONZALEZ', 'ALEJANDRA', 3 , 'LOGA7111209TO', 'LOGA711120MMCPNL05');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'CAMPOS', 'OLMOS', 'ALEJANDRA', 2 , 'CAOA630818IM7', 'CAOA630818MYNMLL09');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'JIMENEZ', 'LOPEZ', 'DAVID', 3 , 'JILD6502061FB', 'JILD650206HBCMPV14');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'SANCHEZ', 'GONZALEZ', 'ALEJANDRA', 2 , 'SAGA620310XUS', 'SAGA620310MCLNNL07');
+
+insert into empleado
+(empleado_id, jefe_id, foto, apellido_paterno,apellido_materno, nombre,puesto_id, rfc, curp)
+values
+(seq_empleado_id.nextval,null,null, 'GODINEZ', 'CAMPOS', 'SARA', 2 , 'GOCS770617YA3', 'GOCS770617MPLDMR14');
